0e71aa254590a3c541643f93da28483de677db1e
jalal-codr <abduljalal849@gmail.com>
2024-02-29 18:12:29 +0100

    updates on navigation

--- Screens/Home/Home.jsx
+++ Screens/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text,Button} from 'react-native'
 
-function Home() {
+
+function Home({navigation}) {
+
+  const btnPress  = ()=>{
+    navigation.navigate('Signin');
+  }
   return (
   <View>
     <Text>Hello</Text>
+    <Button title='SignIn' onPress={btnPress}/>
   </View>
   )
 }
--- Screens/Signin/Signin.jsx
+++ Screens/Signin/Signin.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text,Button} from 'react-native'
 
-function Signin() {
+
+function Signin({navigation}) {
+    const goBack = ()=>{
+        navigation.popToTop();
+    }
+    const btnClick = ()=>{
+        navigation.push('Signin')
+    }
   return (
     <View>
         <Text>SignIn Page</Text>
+        <Button title='Signin' onPress={btnClick}/>
+        <Button title='Back Home' onPress={goBack}/>
     </View>
   )
 }
